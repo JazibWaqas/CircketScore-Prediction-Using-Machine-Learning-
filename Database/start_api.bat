104c4baa3f7031057546ca54916132f6a9a0ce0c
JazibWaqas <jazib.waqas@gmail.com>
2025-10-06 10:01:18 +0500

    Repo kinda clean now

--- Database/start_api.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Cricket Prediction API Server...
-echo.
-echo Database: cricket_prediction.db
-echo Players: 8,321 T20 players
-echo API: http://localhost:5000
-echo.
-python app.py
-pause
